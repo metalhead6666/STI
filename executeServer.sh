eb0eb951fcd62f48515667575e409fd09be29415
metalhead_6666 <noe.godinho.92@gmail.com>
2016-05-20 15:54:43 +0100

    Command to generate keys

--- executeServer.sh
+++ executeServer.sh
@@ -1,1 +1,2 @@
+#keytool -genkey -keystore yourKEYSTORE -keyalg RSA
 java -Djavax.net.ssl.keyStore=bla.key -Djavax.net.ssl.keyStorePassword=stisti -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol -Djavax.net.debug=ssl ChatServer 8888
